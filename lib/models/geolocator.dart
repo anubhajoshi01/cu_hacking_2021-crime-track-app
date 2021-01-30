b9547e977cd168968ec94f7fa2fe94f42faad610
Anubha Joshi <anubhajoshi01@gmail.com>
2021-01-30 18:50:06 -0500

    got latitude longitude working

--- lib/models/geolocator.dart
+++ lib/models/geolocator.dart
@@ -4,32 +4,13 @@ class Geolocation {
   static final Geolocator geolocator = Geolocator()
     ..forceAndroidLocationManager;
 
-  static Position getCurrentLocation() {
-    geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      return position;
-    }).catchError((e) {
-      print(e);
-      return null;
-    });
+  static Future<Position> getCurrentLocation() async{
+    Position p = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return p;
   }
 
-  static String getAddressFromLatLng() async {
-    Position pos = getCurrentLocation();
-    try {
-      List<Placemark> p = await geolocator.placemarkFromCoordinates(
-          pos.latitude, pos.longitude);
-      Placemark place = p[0];
-        return "${place.locality}, ${place.postalCode}, ${place.country}";
-    } catch (e) {
-      print(e);
-      return "Error getting location";
-    }
-  }
-
-  static String getAddressFromLatLng() async {
-    Position pos = getCurrentLocation();
+  static Future<String> getAddressFromLatLong() async {
+    Position pos = await getCurrentLocation();
     try {
       List<Placemark> p = await geolocator.placemarkFromCoordinates(
           pos.latitude, pos.longitude);
@@ -39,5 +20,13 @@ class Geolocation {
       print(e);
       return "Error getting location";
     }
+  }
+
+  static Future<List<double>> getLatLong() async{
+    Position pos = await getCurrentLocation();
+    List<double> latlong = new List();
+    latlong.add(pos.latitude);
+    latlong.add(pos.longitude);
+    return latlong;
   }
 }
